74f6c72b8134e5ae2499ba076090d1dce80c525b
Chowa <arfinchowa524@gmail.com>
2024-03-26 20:54:47 +0600

    css added for the navlink

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,17 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
     <div>       
       <nav>
       <span>My Website: </span>
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link>
         <Link to="/users">Users</Link>
         <Link to="/posts">Posts</Link>
         <Link to="/about">About</Link>
-        <Link to="/contact">Contact us</Link>
-      
-       
+        <Link to="/contact">Contact us</Link> */}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact us</NavLink>
       </nav>
     </div>
   );
